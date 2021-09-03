b896d4051728978532d2d67e2495ffe0a30d49e5
Willian Jean <willianjean@gmail.com>
2021-09-03 14:29:56 -0400

    adicionado viewbinding, algumas extensions e um datepicker do material design

---
+++ app/src/main/java/br/com/dio/todolist/ui/AddTaskActivity.kt
@@ -0,0 +1,34 @@
+package br.com.dio.todolist.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.dio.todolist.databinding.ActivityAddTaskBinding
+import br.com.dio.todolist.extensions.format
+import br.com.dio.todolist.extensions.text
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.util.*
+
+class AddTaskActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddTaskBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.tilDate.editText?.setOnClickListener{
+            val datePicker = MaterialDatePicker.Builder.datePicker().build()
+            datePicker.addOnPositiveButtonClickListener {
+                val timeZone = TimeZone.getDefault()
+                val offset = timeZone.getOffset(Date().time) * -1
+                binding.tilDate.text = Date(it + offset).format()
+            }
+            datePicker.show(supportFragmentManager, "DATE_PICKER_TAG")
+        }
+    }
+}
